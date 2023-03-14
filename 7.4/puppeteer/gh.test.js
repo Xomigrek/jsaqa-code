11a2e004054afc391b4ec2c09a9a715ab1a8d6d4
Igor Khomenko <xomigrek@gmail.com>
2023-03-14 20:10:13 +0300

    7.4. Puppeteer 1

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -1,8 +1,6 @@
 let page;
-
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
@@ -10,12 +8,17 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
+  beforeEach(async () => {
+  await page.goto("https://github.com/team", {timeout: 60000});
+});
+
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector('h1');
     const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
+    expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
   });
 
   test("The first link attribute", async () => {
@@ -29,6 +32,27 @@ describe("Github page tests", () => {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
+    expect(actual).toContain("Get started with Team")
+  });
+});
+
+describe("Another page tests", () => {
+  test("The h1 header enterprise'", async () => {
+    await page.goto("https://github.com/enterprise", {timeout: 60000});
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('Enterprise · A smarter way to work together · GitHub');
+  });
+  test("The h1 header education'", async () => {
+    await page.goto("https://education.github.com/", {timeout: 60000});
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('Engaged students are the result of using real-world tools - GitHub Education');
+  }, 60000);
+  test("The h1 header topics'", async () => {
+    await page.goto("https://github.com/topics", {timeout: 60000});
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('Topics on GitHub · GitHub');
   });
 });
